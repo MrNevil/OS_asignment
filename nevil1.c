b49eb32d8d5c323a416b73bddedca18eb8138219
Danish Ali <1712danish>
2020-04-03 14:40:50 +0530

    Child function added

--- nevil1.c
+++ nevil1.c
@@ -15,6 +15,29 @@ void parent()
 	
 	
 }
+void child(int argc,char *argv[])
+{
+	int fd, i, ch; 
+    
+    for (i = 1; i < argc; i++) {                    /*iterate the loop for total argument count.*/
+        
+        fd = open(argv[1],O_RDONLY);            /*open the file in READONLY mode*/
+        
+        if(fd < 0) { 
+            perror("open"); 
+            goto OUT; 
+        } 
+        
+        while(read(fd,&ch,1))                   /*Read one byte at a time*/
+            
+            write(STDOUT_FILENO,&ch,1);     /*Write the byte on Stdout*/
+        
+        close(fd);                              /*close the file discriptor*/
+    }
+    OUT: 
+        return -1;
+	
+}
 
  
 int main()
